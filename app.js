f8cb3016584a5d689e68c6157a3c282b22ac062a
hohoyha <hohoyha@hotmail.com>
2017-03-19 02:37:53 +0900

    keypress Move

--- app.js
+++ app.js
@@ -11,21 +11,60 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
 
 var SOKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id) {
+    var self = {
+        x:250,
+        y:250,
+        id:id,
+        number:"" + Math.floor(10 * Math.random()),
+        pressRight:false,
+        pressLeft:false,
+        pressUp:false,
+        pressDown:false,
+        maxSpd:10,
+    }
+
+    self.updatePosition = function(){
+        if(self.pressRight)
+            self.x += self.maxSpd;
+        if(self.pressLeft)
+            self.x -= self.maxSpd;
+        if(self.pressUp)
+            self.y += self.maxSpd;
+        if(self.pressDown)
+            self.y -= self.maxSpd;
+    }
+
+    return self;
+}
+
 
 
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
     
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = "" + Math.floor(10 
-    * Math.random());
-
     SOKET_LIST[socket.id] = socket;
+
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
     
     socket.on('disconnect', function() {
         delete SOKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
+    });
+
+    socket.on('keyPress', function(data){
+        if(data.inputId === 'left' )
+            player.pressLeft = data.state;
+        else if(data.inputId === 'right' )
+            player.pressRight = data.state;
+        else if(data.inputId === 'up' )
+            player.pressUp = data.state;
+        else if(data.inputId === 'down' )
+            player.pressDown = data.state;
     });
 
 });
@@ -33,14 +72,13 @@ io.sockets.on('connection', function(socket){
 setInterval(function() {
 
     var pack =[];
-    for( var i in SOKET_LIST){
-        var socket = SOKET_LIST[i];
-        socket.x++;
-        socket.y++;
+    for( var i in PLAYER_LIST){
+        var player = PLAYER_LIST[i];
+        player.updatePosition(),
         pack.push({
-            x:socket.x,
-            y:socket.y,
-            number:socket.number       
+            x:player.x,
+            y:player.y,
+            number:player.number       
         });
     }
 
