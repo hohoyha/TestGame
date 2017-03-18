a8fcb27e3e2766960644f9187b4d8630a0f172fd
hohoyha <hohoyha@hotmail.com>
2017-03-18 23:21:06 +0900

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var serv = require('http').createServer(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+    console.log('socket connection');
+
+    socket.on('happy', function(data){
+        console.log(data);
+    });
+
+    socket.emit('serverMsg',{
+        msg:'hello Hoo'
+    });
+});
