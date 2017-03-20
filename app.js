235a1678a5c683fb2a0267242b34a9f922b4f52c
hohoyha <hohoyha@hotmail.com>
2017-03-20 15:15:49 +0900

    img added

--- app.js
+++ app.js
@@ -139,6 +139,7 @@ Player.onConnect = function(socket){
 
 
     socket.emit('init', {
+        selfId:socket.id,
         player:Player.getAllInitPack(),
         bullet:Bullet.getAllInitPack(),
     })
